4bd001372557add4b76741c1a7b00627f9ab43b2
quiceno <quicenolondonoj@gmail.com>
2023-03-18 11:06:46 -0500

    validar.py

--- validar.py
+++ validar.py
@@ -5,3 +5,16 @@ bienvenido al registro de usuarios, llene los campos que soliciten y seleccione 
 [3]digite su correo
 """
 print(menu)
+opcion=input('entre la opcion 1, 2 o 3: ')
+if opcion=='1':
+    nombre=input("diga su nombre ")
+    if nombre.isalpha():
+        print("su nombre es: ", nombre)
+    else:
+        print('su nombre esta mal digitado')
+elif opcion=='2':
+    edad=input('digite su edad')
+    if edad.isalnum():
+        print('su edad es: ',edad)
+    else: 
+        print('su edad esta mal digitada')
